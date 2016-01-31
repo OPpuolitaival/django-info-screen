875f444f83ec9d7aad564c8acf14b97c0166f9dd
Olli-Pekka Puolitaival <olli-pekka.puolitaival@indooratlas.com>
2016-01-31 22:01:15 +0200

    Django 1.9 related debug fix

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 admin.autodiscover()
@@ -9,3 +10,7 @@ urlpatterns = [
     url(r'^info_screen/', include('info_screen.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
