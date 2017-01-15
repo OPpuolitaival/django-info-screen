d9a71c2eb80be8df0a7e437effcedca7d5f805a2
Olli-Pekka Puolitaival <olli-pekka@puolitaival.fi>
2017-01-15 13:27:39 +0200

    new version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-info_screen',
-    version='0.3',
+    version='0.8',
     description='Django simple info screen application',
     author='Olli-Pekka Puolitaival',
     author_email='oopee1@gmail.com',
